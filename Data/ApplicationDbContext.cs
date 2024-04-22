5adc915140ae1c1ee76a1d7f0d3f548dc88f6211
Yakup Budak <144778514+YakupBudak@users.noreply.github.com>
2024-04-22 19:24:36 +0200

    update 1

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Prename { get; set; }
+
+        public string Lastname { get; set; }
+    }
+}
